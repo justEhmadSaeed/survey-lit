951dbf00d9493f3b6d2a457d2a2604ebdad04dca
Ehmad Saeed <justehmadsaeed@gmail.com>
2022-01-18 22:32:40 +0500

    📦 NEW: Add tailwind directives

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React from 'react'
 import './App.css';
 
 const App = () => {
-	return <div></div>;
+	return (
+		<h1 className="text-3xl font-bold underline">Hello world!</h1>
+	);
 };
 export default App;
