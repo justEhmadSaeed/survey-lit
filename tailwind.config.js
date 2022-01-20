e2c321f066d111ed9a40833bf5989afee9fe2cf2
Ehmad Saeed <justehmadsaeed@gmail.com>
2022-01-20 22:14:41 +0500

    Add template colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
 	content: ['./src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'template-black': '#262627'
+			}
+		},
+		fontFamily: {
+			Poppins: ['Poppins, sans-serif']
+		}
 	},
 	plugins: []
 };
