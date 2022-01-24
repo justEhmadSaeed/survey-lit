a5eca87e1d8ce4042e0f8dba6c4dc97a029741d9
Ehmad Saeed <justehmadsaeed@gmail.com>
2022-01-25 00:33:34 +0500

    📦 NEW: Create Login Page

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,7 +1,117 @@
+import AuthDivider from 'components/Auth/AuthDivider';
+import AuthNavbar from 'components/Auth/AuthNavbar';
+import AuthTile from 'components/Auth/AuthTile';
 import React from 'react';
+import { signInWithGoogle } from 'services/firebase/firebase';
+import {
+	TYPE_EMAIL,
+	TYPE_PASSWORD
+} from 'utils/constants/INPUT_TYPE.constant';
+import {
+	PATH_HOME,
+	PATH_SIGNUP
+} from 'utils/constants/ROUTING_PATHS.constant';
+import typeform_logo from 'assets/typeform_logo.svg';
+import google_icon from 'assets/google_icon.svg';
+import microsoft_icon from 'assets/microsoft_icon.svg';
+import TextField from 'components/TextField';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-	return <div>Login</div>;
+	return (
+		<div className="flex relative flex-col flex-1 basis-1/3 w-full h-full max-h-full bg-white text-template-signup-text">
+			{/* Navigation */}
+			<AuthNavbar
+				helperText="Don't have an account yet?"
+				path={PATH_SIGNUP}
+				title="Sign up"
+			/>
+			{/* Content Container */}
+			<div className="relative overflow-y-auto w-full h-full">
+				<div className="box-inherit">
+					{/* Logo */}
+					<div className="flex items-center justify-center h-20">
+						<Link to={PATH_HOME}>
+							<img src={typeform_logo} alt="Logo" />
+						</Link>
+					</div>
+					{/* Login Module */}
+					<div className="box-inherit">
+						<main className="w-96 h-auto mx-auto mb-2">
+							{/* Auth Content */}
+							<div className="px-16 pb-5">
+								<div className="relative flex flex-col items-center">
+									<div className="w-full">
+										<h2 className="text-center text-lg text-template-signup-text mb-6">
+											Hello, who&apos;s this?
+										</h2>
+										{/* Login Fields */}
+										<div>
+											<p>Email</p>
+											<TextField
+												type={TYPE_EMAIL}
+												placeholder="bruce@wayne.com"
+											/>
+											<p>Password</p>
+											<TextField
+												type={TYPE_PASSWORD}
+												placeholder="At least 8 characters"
+											/>
+										</div>
+										<Link
+											to={PATH_HOME}
+											className="text-sm text-template-auth-text underline mb-4"
+										>
+											Forgot Password?
+										</Link>
+										<button
+											className="my-6 w-full h-10 bg-template-black text-white text-sm rounded cursor-pointer btn hover:opacity-90"
+											onClick={signInWithGoogle}
+										>
+											Log in to Typeform
+										</button>
+									</div>
+									{/* <div className=" flex flex-col items-center"></div> */}
+									{/* Auth Container */}
+									<div className="w-full">
+										{/* Auth Divder */}
+										<AuthDivider />
+										{/* Auth Modules */}
+										<div className="flex flex-col justify-center">
+											<AuthTile
+												title="Sign in with Google"
+												imgSrc={google_icon}
+												onClick={
+													signInWithGoogle
+												}
+											/>
+											<AuthTile
+												title="Sign in with Microsoft"
+												imgSrc={
+													microsoft_icon
+												}
+												onClick={
+													signInWithGoogle
+												}
+											/>
+											<button
+												onClick={
+													signInWithGoogle
+												}
+												className="text-xs text-template-auth-text my-2 underline"
+											>
+												Sign In with SSO
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</main>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
