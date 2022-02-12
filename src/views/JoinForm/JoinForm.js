15f1a9730e3ab1b1f8b7abc1f257bef7a6475d02
Ehmad Saeed <justehmadsaeed@gmail.com>
2022-02-12 17:39:22 +0500

    📦 NEW: Design form navigation footer

--- src/views/JoinForm/JoinForm.js
+++ src/views/JoinForm/JoinForm.js
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getFormData } from 'utils/form-data/form-data';
 import Loading from 'views/Loading/Loading';
 import CurrentQuestion from 'views/JoinForm/CurrentQuestion';
+import { PATH_HOME } from 'utils/constants/routing-paths.constant';
+import {
+	ChevronDownIcon,
+	ChevronUpIcon
+} from '@heroicons/react/solid';
 
 // Starting Point of Join Form
 const JoinForm = () => {
@@ -46,6 +51,25 @@ const JoinForm = () => {
 								question.id === currentQuestion.id
 						)}
 					/>
+				</div>
+			</div>
+			<div className="fixed w-full flex justify-end p-4 bottom-0">
+				{/* Buttons Wrapper */}
+				<div className="w-full flex justify-between md:justify-end ">
+					<nav className="flex mr-2 text-white">
+						<button className="border-r border-white py-1 px-2 rounded-l transition duration-300 cursor-pointer bg-blue-800 hover:opacity-80">
+							<ChevronUpIcon className="h-6 w-6" />
+						</button>
+						<button className="py-1 px-2 rounded-r transition duration-300 cursor-pointer bg-blue-800 hover:opacity-80">
+							<ChevronDownIcon className="h-6 w-6" />
+						</button>
+					</nav>
+					<Link
+						to={PATH_HOME}
+						className="btn bg-blue-800 text-white"
+					>
+						Powered by Typeform
+					</Link>
 				</div>
 			</div>
 		</div>
