b865af73fe405ce833901e04563dbd482cf4517e
Ehmad Saeed <justehmadsaeed@gmail.com>
2022-02-12 18:10:30 +0500

    Navigation functions

--- src/views/JoinForm/JoinForm.js
+++ src/views/JoinForm/JoinForm.js
@@ -16,6 +16,12 @@ const JoinForm = () => {
 	const [error, setError] = useState();
 	const [questions, setQuestions] = useState([]);
 	const [currentQuestion, setCurrentQuestion] = useState();
+	let currentQuestionIndex =
+		currentQuestion &&
+		questions.findIndex(
+			(question) => question.id === currentQuestion.id
+        );
+    
 
 	useEffect(() => {
 		const getData = async () => {
@@ -33,6 +39,12 @@ const JoinForm = () => {
 		};
 		getData();
 	}, []);
+	const onNextQuestionClick = () => {
+		setCurrentQuestion(questions[currentQuestionIndex + 1]);
+	};
+	const onPreviousQuestionClick = () => {
+		setCurrentQuestion(questions[currentQuestionIndex - 1]);
+	};
 	return loading ? (
 		<Loading />
 	) : error ? (
@@ -46,21 +58,39 @@ const JoinForm = () => {
 				<div className="px-10 text-left">
 					<CurrentQuestion
 						question={currentQuestion}
-						index={questions.findIndex(
-							(question) =>
-								question.id === currentQuestion.id
-						)}
+						index={currentQuestionIndex}
 					/>
 				</div>
 			</div>
+			{/* Form Navigation Footer */}
 			<div className="fixed w-full flex justify-end p-4 bottom-0">
 				{/* Buttons Wrapper */}
 				<div className="w-full flex justify-between md:justify-end ">
 					<nav className="flex mr-2 text-white">
-						<button className="border-r border-white py-1 px-2 rounded-l transition duration-300 cursor-pointer bg-blue-800 hover:opacity-80">
+						<button
+							onClick={onPreviousQuestionClick}
+							disabled={currentQuestionIndex < 1}
+							className={`${
+								currentQuestionIndex < 1
+									? 'text-gray-400'
+									: ''
+							} border-r border-white py-1 px-2 rounded-l transition duration-300 cursor-pointer bg-blue-800 hover:opacity-80`}
+						>
 							<ChevronUpIcon className="h-6 w-6" />
 						</button>
-						<button className="py-1 px-2 rounded-r transition duration-300 cursor-pointer bg-blue-800 hover:opacity-80">
+						<button
+							onClick={onNextQuestionClick}
+							disabled={
+								currentQuestionIndex >=
+								questions.length - 1
+							}
+							className={`${
+								currentQuestionIndex >=
+								questions.length - 1
+									? 'text-gray-400'
+									: ''
+							} py-1 px-2 rounded-r transition duration-300 cursor-pointer bg-blue-800 hover:opacity-80`}
+						>
 							<ChevronDownIcon className="h-6 w-6" />
 						</button>
 					</nav>
